a8fb7e4c92d4981c33c352d89639ef05c8e6cf74
_North <underscorenorth@gmail.com>
2022-12-29 09:13:10 -0500

    Displaying playlist item duration in time rather than seconds

--- src/lib/playlist/PlaylistItem.svelte
+++ src/lib/playlist/PlaylistItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import MdPlayCircleOutline from 'svelte-icons/md/MdPlayCircleOutline.svelte';
+	import { secondsToTime } from '$lib/utilities/timeUtilities';
 	import MdDelete from 'svelte-icons/md/MdDelete.svelte';
 	export let item: any;
 	export let deleteItem: Function;
@@ -15,7 +16,7 @@
 	<td><a href={item.url} target="_blank" rel="noreferrer">{item.name}</a></td>
 	<td>{item.endDate}</td>
 	<td>{item.user}</td>
-	<td>{item.duration}</td>
+	<td>{secondsToTime(item.duration)}</td>
 </tr>
 
 <style>
---
+++ src/lib/utilities/timeUtilities.ts
@@ -0,0 +1,14 @@
+export const secondsToTime = (duration: number) => {
+	let hours = 0,
+		minutes = 0,
+		seconds = 0;
+	if (duration > 60 * 60) hours = Math.floor(duration / 3600);
+	if (duration > 60) minutes = Math.floor(duration / 60) % 60;
+	seconds = duration % 60;
+	return (
+		(hours ? hours.toString().padStart(2, '0') + ':' : '') +
+		minutes.toString().padStart(2, '0') +
+		':' +
+		seconds.toString().padStart(2, '0')
+	);
+};
