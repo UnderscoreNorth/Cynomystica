93f71aef9df60b2779020132da0c65666fc4f596
ALEX-PC\Alex <alex_nguyen@live.ca>
2023-10-05 02:30:08 -0400

    Allowing for endpoint without port number

--- front/src/lib/realtime.ts
+++ front/src/lib/realtime.ts
@@ -1,6 +1,6 @@
 import ioClient from 'socket.io-client';
 import CONFIG from './clientconfig.json';
-const ENDPOINT = `${CONFIG.SERVER_URL}:${CONFIG.SERVER_PORT}`;
+const ENDPOINT = `${CONFIG.SERVER_URL}${CONFIG.SERVER_PORT ? `:${CONFIG.SERVER_PORT}` : ''}`;
 
 const socket = ioClient(ENDPOINT, { transports: ['websocket'], upgrade: false });
 socket.onAny((eventName, ...args) => {
