94fab7c83dd45e324b2123b818741ef323d9e987
ALEX-PC\Alex <alex_nguyen@live.ca>
2023-10-05 03:17:37 -0400

    Die

--- front/src/lib/realtime.ts
+++ front/src/lib/realtime.ts
@@ -1,7 +1,7 @@
 import ioClient from 'socket.io-client';
 import CONFIG from './clientconfig.json';
 const ENDPOINT = `${CONFIG.SERVER_URL}${CONFIG.SERVER_PORT ? `:${CONFIG.SERVER_PORT}` : ''}`;
-
+console.log({ ENDPOINT });
 const socket = ioClient(ENDPOINT, { transports: ['websocket'], upgrade: false });
 socket.onAny((eventName, ...args) => {
 	console.log('onany', eventName, args);
