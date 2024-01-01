afbe5fc10e067596155fa01d4061984fa1ffde90
ALEX-PC\Alex <alex_nguyen@live.ca>
2024-01-01 05:04:30 -0500

    Users last active date fix

--- front/src/lib/realtime.ts
+++ front/src/lib/realtime.ts
@@ -1,9 +1,9 @@
 import ioClient from 'socket.io-client';
 import CONFIG from './clientconfig.json';
-const ENDPOINT = `${CONFIG.SERVER_URL}${CONFIG.SERVER_PORT ? `:${CONFIG.SERVER_PORT}` : ''}`;
+const ENDPOINT = `${CONFIG.SERVER_URL}${CONFIG.SERVER_PORT ? `:${CONFIG.SERVER_PORT}` : ''}/`;
 //console.log({ ENDPOINT });
 const socket = ioClient(ENDPOINT, {
-	path: '/ws',
+	path: 'ws',
 	transports: ['websocket'],
 	upgrade: false
 });
